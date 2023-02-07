28a4850dc41c8020d1ff584d8a4432d686641376
kaviyakarikalan <124652847+11kavya2003@users.noreply.github.com>
2023-02-07 04:54:55 +0000

    Add files via upload

---
+++ Natural.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Natural
+{
+public static void main(String args[ ])
+{
+int n,i;
+for(i=1;i<=n;i++)
+{
+ for( int j=1;j<=n;j++)
+{
+System.out.print(i);
+}
+System.out.println(" ");
+}
+}
+}
